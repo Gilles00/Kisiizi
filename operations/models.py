ad5972bb6773102d93a6d33afad0fd746232743f
unknown <gilles00>
2021-05-10 01:25:01 +0300

    The finished project first commit

---
+++ operations/models.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+from core.models import TimeStampedModel
+from reception.models import Patient
+from hospital.models import Diagnose
+from staff.models import Doctor, Nurse
+
+
+# Create your models here.
+class Diagnose(TimeStampedModel):
+    name = models.CharField(max_length=200)
+    code = models.IntegerField(default=0, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return '{}-{}'.format(self.code, self.name)
+
+
+class Treatment(TimeStampedModel):
+    date = models.DateField(default=timezone.now().date())
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
+    symptoms = models.TextField()
+    diagnosis = models.ForeignKey(Diagnose, on_delete=models.DO_NOTHING)
+    doctors_comments = models.TextField()
+
+    def __str__(self):
+        return '{}-{}'.format(self.patient, self.diagnosis)
+
+
+class Vaccine(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    live = models.NullBooleanField()
+    absorved = models.NullBooleanField()
+    inactivated = models.NullBooleanField()
+    oral = models.NullBooleanField()
+
+    def __str__(self):
+        return self.name
+
+class VaccineApplied(TimeStampedModel):
+    date = models.DateField(default=timezone.now().date())
+    vaccine = models.ForeignKey(Vaccine, on_delete=models.DO_NOTHING)
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    nurse = models.ForeignKey(Nurse, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return '%s | by  %s (%s)' % (self.patient, self.nurse, self.date)
+
+    class Meta:
+        verbose_name = 'Vaccine Applied'
+        verbose_name_plural = 'Vaccines usage'
+
+
+class Test(TimeStampedModel):
+    mame = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class MedicalTest(TimeStampedModel):
+    date = models.DateField(default=timezone.now().date())
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    test = models.ForeignKey(Test, on_delete=models.DO_NOTHING)
+    results = models.CharField(max_length=500)
+    status = models.BooleanField()
+
+    def __str__(self):
+        return '{}-{}'.format(self.patient, self.test.name)
---
+++ reception/models.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.template.defaultfilters import truncatechars
+from django.utils import timezone
+from django.db import models
+
+# project imports
+from core.models import TimeStampedModel
+from hospital.models import Person, Room, Diagnose
+from staff.models import Doctor
+
+
+class Patient(Person):
+    preferred_hospital = models.CharField(max_length=50, blank=True, null=True)
+    primary_physician = models.CharField(max_length=50, blank=True, null=True)
+    last_physical = models.CharField(max_length=50)
+    health_history = models.CharField(max_length=600)
+    status = models.CharField(max_length=10)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class In_patient(TimeStampedModel):
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    date_of_adm = models.DateField(default=timezone.now().date())
+    date_of_discarge = models.DateField()
+    diagnosis = models.ForeignKey(Diagnose, on_delete=models.DO_NOTHING)
+    doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
+    room = models.ForeignKey(Room, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return '{}-{}'.format(self.patient, self.room)
+
+
+class Out_patient(TimeStampedModel):
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    date = models.DateField(default=timezone.now().date())
+    diagnosis = models.ForeignKey(Diagnose, on_delete=models.DO_NOTHING)
+    doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return '{}'.format(self.patient)
+
+
+class MedicalHistory(TimeStampedModel):
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    textHistory = models.CharField(max_length=500)
+
+    @property
+    def brief_history(self):
+        if len(self.textHistory) <= 20:
+            return self.textHistory
+        else:
+            return '{}'.format(truncatechars(self.textHistory, 20))
+
+    def __str__(self):
+        return '{}-{}'.format(self.patient, self.brief_history)
